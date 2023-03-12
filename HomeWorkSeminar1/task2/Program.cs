d8b7513a213e8bc1d84741c74bf5ad3615afd67a
andrei ignat <>
2023-03-12 21:56:54 +0200

    git initial

---
+++ HomeWorkSeminar1/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("а = " + a + " " + "b = " + b + " " + "max = " + a);
+}
+else
+{
+    Console.WriteLine("а = " + a + " " + "b = " + b + " " + "max = " + b);
+}
---
+++ HomeWorkSeminar1/task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n != 1)
+{
+     for ( int i = 0; i <= n; i = i+2)
+     {      
+
+     Console.WriteLine(i);
+     }
+}
+else
+{
+     Console.WriteLine("Число 1");
+}
