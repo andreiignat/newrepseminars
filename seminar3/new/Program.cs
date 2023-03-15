1fde833df79ec57c434404e9d46baf6cb6e2152e
andrei ignat <>
2023-03-15 20:35:14 +0200

    seminar3

---
+++ seminar3/new/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int x1, x2, y1, y2;
+
+Console.Write($"Введите координату х1: ");
+int.TryParse(Console.ReadLine(), out x1);
+
+Console.Write($"Введите координату у1: ");
+int.TryParse(Console.ReadLine(), out y1);
+
+Console.Write($"Введите координату х2: ");
+int.TryParse(Console.ReadLine(), out x2);
+
+Console.Write($"Введите координату у2: ");
+int.TryParse(Console.ReadLine(), out y2);
+
+ double x = (x2-x1)*(x2-x1);
+ double y = (y2-y1)*(y2-y1);
+
+ double res = Math.Sqrt(x + y);
+ Console.WriteLine(res);
