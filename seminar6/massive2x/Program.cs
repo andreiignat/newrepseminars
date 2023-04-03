cfc43867082168e61358582886ee0af243f6399a
andrei ignat <>
2023-04-03 19:34:15 +0300

    seminar6

---
+++ seminar6/massive2x/Program.cs
@@ -0,0 +1,45 @@
+﻿// Двумерный массив
+// Создаем функцию по заполнению двумерного массива
+
+
+int[,] array = new int[3, 4];
+Fill2DArray(array);
+PrintArray(array, "array");
+
+int[,] array2 = Create2DArray(3, 4);
+PrintArray(array2, "array2");
+
+
+
+
+int[,] Create2DArray(int rowCount, int columnCount )
+{
+    int[,] arrayT =new int[rowCount, columnCount];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(10);
+    return array;
+}
+
+
+void Fill2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(10);  
+}
+
+void PrintArray(int[,] array, string title = "")
+{
+    if (title != "")
+    {
+        Console.Write($"{title}\n");
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)    
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
---
+++ seminar6/task39/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Напишите программу РЕВЕРС массива одномерного*/
+
+int[] array = CreateArray(6);
+
+
+PrintArray(array);
+array = ReverseArray(array);
+PrintArray(array);
+
+int[] ReverseArray(int[] arrayT)
+{
+    int[] array2 = new int[arrayT.Length];
+    int i = 0;
+    int j = arrayT.Length -1;
+    while (j >= 0)
+    {
+        array2[i] = arrayT[j];
+        i++;
+        j++;
+
+    }
+    return array2;
+}
+
+int[] CreateArray(int size, int min = 0, int max = 9)
+{
+    int[] arrayT = new int [size];
+    for(int i = 0; i < size; i++)
+        arrayT[i] = new Random().Next(min, max+1);
+    return arrayT;
+}
+
+void PrintArray(int[] arrayT)
+{
+    for(int i = 0; i < arrayT.Length; i++)
+        Console.Write($"{arrayT[i]}");
+    Console.WriteLine();
+}
