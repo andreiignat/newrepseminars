a1516245223139d2798ec9024da9d4adaa79b047
andrei ignat <>
2023-04-06 11:52:34 +0300

    HomeWorkSeminar9

---
+++ HomeWorkSeminar9/task64/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+int CheckNumber(string message)
+{
+    int a;
+    Console.Write(message);
+    int.TryParse(Console.ReadLine(), out a);
+    return a;
+}
+
+int NaturalNumber(int k, int m)
+{
+    if (k == m) return k;
+    else Console.Write($"{NaturalNumber(k, m + 1)}, ");
+    return m;
+}
+
+int k = CheckNumber("Введите число: ");
+if (k < 1)
+{
+    Console.WriteLine("Ввдите положительное число!");
+    return;
+}
+
+
+Console.WriteLine(NaturalNumber(k, 1));
---
+++ HomeWorkSeminar9/task68/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3->A(m, n) = 9
+// m = 3, n = 2->A(m, n) = 29
+
+
+int a, b;
+Console.Write($"Введите первое положительное число: ");
+int.TryParse(Console.ReadLine(), out a);
+
+Console.Write($"Введите второе положительное число: ");
+int.TryParse(Console.ReadLine(), out b);
+
+CallFunctionOfAkkerman(a, b);
+
+void CallFunctionOfAkkerman(int a, int b)
+{
+    Console.Write(FunctionOfAkkerman(a, b));
+}
+
+int FunctionOfAkkerman(int a, int b)
+{
+    if (a == 0)
+    {
+        return b + 1;
+    }
+    else if (b == 0 && a > 0)
+    {
+        return FunctionOfAkkerman(a - 1, 1);
+    }
+    else
+    {
+        return (FunctionOfAkkerman(a - 1, FunctionOfAkkerman(a, b - 1)));
+    }
+}
